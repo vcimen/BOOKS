1d958b0f79b1c94582b3853a1694b4aae1e00ea7
vcimen <vcimen@na.edu>
2021-05-06 05:46:50 -0400

    hello2

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var port = Process.env.PORT || 5000 ;
+var port = 5000 ;
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 require("./app/routes")(app);
 // app.set("port", 3000);
-app.listen(port, function () {
-  console.log("server listening to port "+port);
-});
+app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
+

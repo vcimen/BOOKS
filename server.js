ba7c9e67a413511f50e4fd4bee66a0870bd74502
vcimen <vcimen@na.edu>
2021-05-06 05:10:03 -0400

    Added a Procfile.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var port = 3000;
+var port = Process.env.PORT || 3000 ;
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 require("./app/routes")(app);
 // app.set("port", 3000);
 app.listen(port, function () {
-  console.log("Server up: http://localhost:3000");
+  console.log("server listening to port "+port);
 });
